e9b4846e475e0de8022e01158ae7bcbeb2fb307f
Erceett <omerfercetin@gmail.com>
2025-03-26 15:33:29 +0300

    some fixes

--- app/src/auth/jwt.strategy.ts
+++ app/src/auth/jwt.strategy.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtService } from '@nestjs/jwt';
-import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
